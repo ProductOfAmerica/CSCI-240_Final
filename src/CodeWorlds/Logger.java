473dbb3384d8d7562fc56c6e423e35b2a19b2322
computor <jeff.smith34r32@gmail.com>
2016-04-19 10:46:17 -0500

    Moved files

--- src/CodeWorlds/Logger.java
+++ src/CodeWorlds/Logger.java
@@ -4,7 +4,7 @@ import java.io.PrintStream;
 import java.util.Date;
 
 public class Logger {
-    public static String kLogFile = "CodeWorlds.log";
+    public static String kLogFile = "Files/CodeWorlds.log";
 
     private static Logger singleton;  // Singleton CodeWorlds.Logger object
 
