3dc4949684f6c16e7b37e668f5b8d1a1876b8eb0
computor <jeff.smith34r32@gmail.com>
2016-05-01 12:30:46 -0500

    Added composite body

---
+++ src/CodeWorld/Objects/Shapes/CompositeBody.java
@@ -0,0 +1,25 @@
+package CodeWorld.Objects.Shapes;
+
+import CodeWorld.Objects.Body;
+
+import java.util.Iterator;
+
+/**
+ * Created by Calculus on 5/1/2016.
+ */
+public class CompositeBody implements Body {
+    @Override
+    public Rectangle getBounds() {
+        return null;
+    }
+
+    @Override
+    public Body clone(Vector offset) {
+        return null;
+    }
+
+    @Override
+    public Iterator<Brick> iterator() {
+        return null;
+    }
+}
