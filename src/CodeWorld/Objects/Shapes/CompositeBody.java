306686cd2abab82a40bb468bae62a3493728b1f4
timbassler <tim@bassler.com>
2016-05-01 12:39:58 -0500

    CompositeBody GetBounds

--- src/CodeWorld/Objects/Shapes/CompositeBody.java
+++ src/CodeWorld/Objects/Shapes/CompositeBody.java
@@ -8,9 +8,20 @@ import java.util.Iterator;
  * Created by Calculus on 5/1/2016.
  */
 public class CompositeBody implements Body {
+	 private List<Body> children = new LinkedList<Body>();
+	
     @Override
     public Rectangle getBounds() {
-        return null;
+		Rectangle bounds = null;
+        bounds = null;
+        for (Body child : this.children) {
+            if (bounds == null) {
+                bounds = new Rectangle(child.getBounds());
+                continue;
+            }
+            bounds.unionBy(child.getBounds());
+        }
+		return bounds;
     }
 
     @Override
