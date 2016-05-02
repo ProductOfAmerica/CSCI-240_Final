43f69e5e29af59e2e759543a9ccdb506cbdb59b9
computor <jeff.smith34r32@gmail.com>
2016-05-02 02:43:24 -0500

    Final push. Completed project.

--- src/CodeWorld/Drivers/Helpers/AutoWorldFactory.java
+++ src/CodeWorld/Drivers/Helpers/AutoWorldFactory.java
@@ -24,6 +24,53 @@ public class AutoWorldFactory implements WorldFactory {
         bodyList.add(new Stone(new Vector()));
         bodyList.add(new Water(new Vector()));
         bodyList.add(new Unicorn(new Vector()));
+
+        CompositeBody lake = new CompositeBody();
+        lake.add(new Water(new Vector(0,0)));
+        lake.add(new Water(new Vector(0,1)));
+        lake.add(new Water(new Vector(1,0)));
+        lake.add(new Water(new Vector(1,1)));
+        lake.add(new Water(new Vector(0,2)));
+        lake.add(new Water(new Vector(1,2)));
+        lake.add(new Water(new Vector(2,0)));
+        lake.add(new Water(new Vector(2,1)));
+        bodyList.add(lake);
+
+        CompositeBody boulder = new CompositeBody();
+        boulder.add(new Stone(new Vector(0,0)));
+        boulder.add(new Stone(new Vector(1,0)));
+        boulder.add(new Stone(new Vector(1,1)));
+        boulder.add(new Stone(new Vector(0,1)));
+        bodyList.add(boulder);
+
+
+        CompositeBody stand = new CompositeBody();
+        stand.add(new Tree(new Vector(0, 0)));
+        stand.add(new Tree(new Vector(1, 2)));
+        stand.add(new Tree(new Vector(3, 3)));
+        stand.add(new Tree(new Vector(1, 3)));
+        bodyList.add(stand);
+
+        CompositeBody forest = new CompositeBody();
+        forest.add(stand.clone(new Vector(0, 0)));
+        forest.add(stand.clone(new Vector(0, 4)));
+        forest.add(stand.clone(new Vector(4, 0)));
+        forest.add(stand.clone(new Vector(4, 4)));
+        bodyList.add(forest);
+
+        CompositeBody rockField = new CompositeBody();
+        rockField.add(boulder.clone(new Vector(0, 0)));
+        rockField.add(new Stone(new Vector(3, 2)));
+        rockField.add(new Stone(new Vector(1, 3)));
+        bodyList.add(rockField);
+
+        CompositeBody mine = new CompositeBody();
+        mine.add(rockField.clone(new Vector(0,0)));
+        mine.add(new Ore(new Vector(2, 0)));
+        mine.add(new Ore(new Vector(2, 1)));
+        mine.add(new Ore(new Vector(1, 2)));
+        bodyList.add(mine);
+
     }
 
     @Override
@@ -35,8 +82,8 @@ public class AutoWorldFactory implements WorldFactory {
     public WorldFactory build() throws CWSException {
         CompositeBody world = new CompositeBody();
         int failcount = 0;
-        while (failcount < 10) {
-            Body randomBody = bodyList.get( new Random().nextInt(8));
+        while (failcount < 50) {
+            Body randomBody = bodyList.get( new Random().nextInt(bodyList.size()));
             if (!world.add(randomBody.clone(new Vector(new Random().nextInt(50), new Random().nextInt(50)))))
                 failcount++;
         }
