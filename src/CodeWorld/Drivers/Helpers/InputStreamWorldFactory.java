7e3af7d26f628f430b109f3abf8f6f268d87d498
computor <jeff.smith34r32@gmail.com>
2016-05-02 00:23:26 -0500

    Cleaned code

--- src/CodeWorld/Drivers/Helpers/InputStreamWorldFactory.java
+++ src/CodeWorld/Drivers/Helpers/InputStreamWorldFactory.java
@@ -24,14 +24,14 @@ public class InputStreamWorldFactory implements WorldFactory {
     public InputStreamWorldFactory(Object p0) {
         in = new Scanner((InputStream) p0);
 
-        bodyMap.put("Cow", new Cow(new Vector()));
-        bodyMap.put("Ore", new Ore(new Vector()));
-        bodyMap.put("Horse", new Horse(new Vector()));
-        bodyMap.put("Sloth", new Sloth(new Vector()));
-        bodyMap.put("Tree", new Tree(new Vector()));
-        bodyMap.put("Stone", new Stone(new Vector()));
-        bodyMap.put("Water", new Water(new Vector()));
-        bodyMap.put("Unicorn", new Unicorn(new Vector()));
+        bodyMap.put("Cow", new Cow(new Vector())); //Store a new entity in the bodyMap
+        bodyMap.put("Ore", new Ore(new Vector())); //Store a new entity in the bodyMap
+        bodyMap.put("Horse", new Horse(new Vector())); //Store a new entity in the bodyMap
+        bodyMap.put("Sloth", new Sloth(new Vector())); //Store a new entity in the bodyMap
+        bodyMap.put("Tree", new Tree(new Vector())); //Store a new entity in the bodyMap
+        bodyMap.put("Stone", new Stone(new Vector())); //Store a new entity in the bodyMap
+        bodyMap.put("Water", new Water(new Vector())); //Store a new entity in the bodyMap
+        bodyMap.put("Unicorn", new Unicorn(new Vector())); //Store a new entity in the bodyMap
     }
 
     @Override
@@ -41,29 +41,25 @@ public class InputStreamWorldFactory implements WorldFactory {
 
     @Override
     public WorldFactory build() throws CWSException {
-        while (in.hasNext("\\(")){
-            in.next();
-            bodyMap.put(in.next().trim(), getInput());
-            if(in.hasNext() && in.next().contains(")"))
-                continue;
-            throw new CWSException("Missing parenthesis");
+        while (in.hasNext("\\(")){ //While there's more input block patterns in the file....
+            in.next(); //Remove the parenthesis
+            bodyMap.put(in.next().trim(), getInput()); //Get the name, and then get all of the method body code
+            if(!(in.hasNext() && in.next().contains(")"))) //If there's another block, continue
+                throw new CWSException("Missing parenthesis"); //Otherwise, there's a missing block parenthesis
         }
 
-        world = getInput();
+        world = getInput(); //Set the world equal to the Global Variables in the input file
         return this;
     }
 
     private Body getInput() throws CWSException {
-        CompositeBody body = new CompositeBody();
+        CompositeBody body = new CompositeBody(); //New pattern body is created
         while (in.hasNext() && !in.hasNext("\\)")){
             String tempName = in.next().trim(); //This will be the object type
             Vector tempVector = new Vector(in.nextInt(), in.nextInt());
-            if(bodyMap.containsKey(tempName)){ //Validate that the name exists in the bodyMap
-                if(body.add(bodyMap.get(tempName).clone(tempVector))) //Add it, if one does
-                    continue;
+            if((bodyMap.containsKey(tempName) && !body.add(bodyMap.get(tempName).clone(tempVector))))//Validate that the name exists in the bodyMap
                 throw new CWSException(String.format("Exception: Body's (%s) location (%s) already exists.",
                         tempName, tempVector.toString())); //Else, the classes already equal, or locations equal
-            }
         }
         return body;
     }
